4719962bbe0e7bd3883aeaeb5c5a86084d18ea60
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-11-05 14:17:00 -0600

    Use key character for options

--- Program.cs
+++ Program.cs
@@ -8,20 +8,21 @@ Console.WriteLine("1. Display all blogs");
 Console.WriteLine("2. Add blog");
 Console.WriteLine("3. Create post");
 Console.WriteLine("4. Display posts");
-int option = Convert.ToInt32(Console.ReadLine());
+char key = Console.ReadKey().KeyChar;
 var db = new DataContext();
-switch (option)
+switch (key)
 {
-  case 1:
+  case '1':
     // Display all Blogs from the database
     var query = db.Blogs.OrderBy(b => b.Name);
+    Console.WriteLine($"{query.Count()} blogs in database");
     Console.WriteLine("All blogs in the database:");
     foreach (var item in query)
     {
       Console.WriteLine(item.Name);
     }
     break;
-  case 2:
+  case '2':
     // Create and save a new Blog
     Console.Write("Enter a name for a new Blog: ");
     var name = Console.ReadLine();
