c9db8830c61175958f91f76430a01b62ff33aa42
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-11-08 20:10:54 -0600

    Add method to add post and display posts

--- DataContext.cs
+++ DataContext.cs
@@ -11,6 +11,11 @@ public class DataContext : DbContext
     this.Blogs.Add(blog);
     this.SaveChanges();
   }
+  public void AddPost(Post post)
+  {
+    this.Posts.Add(post);
+    this.SaveChanges();
+  }
 
   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
   {
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using NLog;
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using NLog;
 string path = Directory.GetCurrentDirectory() + "//nlog.config";
 // create instance of Logger
 var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
@@ -11,6 +13,7 @@ while (true)
   Console.WriteLine("3. Create post");
   Console.WriteLine("4. Display posts");
   char key = Console.ReadKey().KeyChar;
+  Console.WriteLine();
   if (key == 'q')
   {
     break;
@@ -42,17 +45,59 @@ while (true)
 
       // Create and save a new Post from a Blog
       Console.WriteLine("Select Blog you would like to post:");
-      int id = Convert.ToInt32(Console.ReadLine());
       foreach (var item in db.Blogs)
       {
         Console.WriteLine($"{item.BlogId}. {item.Name}");
       }
-      var blog = db.Blogs.First(b => b.BlogId == id);
+      int id = Convert.ToInt32(Console.ReadLine());
+      var blogging = db.Blogs.First(b => b.BlogId == id);
       Console.WriteLine("Enter Post title:");
       string? title = Console.ReadLine();
       Console.WriteLine("Enter Post content:");
       string? content = Console.ReadLine();
-      blog.Posts?.Add(new Post { Title = title, BlogId = id, Content = content })
+      var post = new Post { Title = title, Content = content, Blog = blogging };
+      db.AddPost(post);
       break;
 
+    case '4':
+      // Display all posts
+      Console.WriteLine("Select Blog's Posts to display:");
+      Console.WriteLine("0. Posts from all blogs");
+      foreach (var item in db.Blogs)
+      {
+        Console.WriteLine($"{item.BlogId}. Posts from {item.Name}");
+      }
+      int ids = Convert.ToInt32(Console.ReadLine());
+      if (ids == 0)
+      {
+        int postCount = db.Posts.Count();
+        Console.WriteLine($"{postCount} posts returned");
+        foreach (var poster in db.Posts)
+        {
+          Console.WriteLine($"Blog: {poster.Blog}");
+          Console.WriteLine($"Title: {poster.Title}");
+          Console.WriteLine($"Content: {poster.Content}");
+
+
+        }
+      }
+      else
+      {
+        var bloggings = db.Posts.Where(x => x.BlogId == ids);
+
+        int postCount = bloggings.Count();
+        Console.WriteLine($"{postCount} posts returned");
+        foreach (var poster in bloggings)
+        {
+          Console.WriteLine($"Blog: {poster.Blog}");
+          Console.WriteLine($"Title: {poster.Title}");
+          Console.WriteLine($"Content: {poster.Content}");
+        }
+
+
+
+      }
+      db.SaveChanges();
+      break;
   }
+}
